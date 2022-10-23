87ccf0288a9a37ca9b4ab43edc22a5cca4b2adab
Gordonias <g.j.d.arscott@tilburguniversity.edu>
2022-10-23 10:22:52 +0200

    Making it so it passes again

--- example_answers.py
+++ example_answers.py
@@ -8,7 +8,7 @@ def fix_is_store_open(current_time, opening_times):
     end = opening_time_list[1]
     # you can also explictly convert to int, strings work though:
     if current_time > '2359' or start > '2359' or end > '2359':
-        return 'invalid stime'
+        return 'invalid time'
     else:
         return start <= current_time <= end
 
